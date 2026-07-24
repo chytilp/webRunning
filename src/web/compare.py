9145973bf9b0c7cf245f10cb1312413561af94f6
Patrik Chytil <pchytil@elektroline.cz>
2026-07-24 11:42:59 +0200

    dashboard and compare forms

--- src/web/compare.py
+++ src/web/compare.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from http.client import HTTPException
 from typing import Any
 
 from fastapi import APIRouter
@@ -5,7 +7,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 
-from libRunning import get_routes, RouteModel, get_compare, CompareModel
+from libRunning import get_routes, RouteModel, get_compare, CompareModel, get_dates
 
 from src.model.compare import Difference, CompareObj
 from src.model.training import Training
@@ -13,12 +15,64 @@ from src.model.training import Training
 router = APIRouter(prefix = "/compare")
 templates = Jinja2Templates(directory="src/templates")
 
+def get_months_items(dates: list[str]) -> dict[str, list[str]]:
+    output = defaultdict(list)
+    for date in dates:
+        output[date[:7]].append(date)
+    return output
+
+def get_max_items(months_items: dict[str, list[str]]) -> int:
+    return max(len(items) for items in months_items.values())
+
+def create_objects_for_template(dates: list[str]) -> dict[str, Any]:
+    months_items = get_months_items(dates)
+    rows = get_max_items(months_items)
+    rows_list = list(range(1,rows+1))
+    data: dict[tuple[str, int], str] = {}
+    for month, values in months_items.items():
+        values = sorted(values)
+        for row in rows_list:
+            index = row - 1
+            try:
+                value = values[index]
+            except IndexError:
+                value = ""
+            data[(month, row)] = value
+
+    return {
+        "months": sorted(list(months_items.keys())),
+        "rows": rows_list,
+        "data": data,
+    }
+
 
 @router.get("", response_class=HTMLResponse)
-def show_compare(request: Request, route: str, dates: str) -> Any:
-    dates_list = dates.split(",")
-    date_1: str = dates_list[0]
-    date_2: str = dates_list[1]
+def show_compare(request: Request, route: str) -> Any:
+    routes: list[RouteModel] = get_routes()
+    route_obj: RouteModel = RouteModel(name=route, description="")
+    routes_list = [{"name": route.name, "description": route.description} for route in routes]
+    dates_obj = get_dates(route_obj)
+    dates = dates_obj.dates
+    data_for_template: dict[str, Any] = create_objects_for_template(dates)
+
+    return templates.TemplateResponse(
+        request=request, name="compareForm.html",
+        context={"route_name": route, "routes": routes_list, "months": data_for_template["months"],
+                 "rows": data_for_template["rows"], "data": data_for_template["data"]},
+    )
+
+@router.post("", response_class=HTMLResponse)
+async def run_compare(request: Request, route: str) -> Any:
+    form = await request.form()
+    dates: list[str] = []
+    for (k, v) in form.items():
+        if k.startswith("chk_"):
+            dates.append(v)
+    if len(dates) != 2:
+        raise ValueError(f"For compare must be selected just 2 dates.")
+
+    date_1: str = dates[0]
+    date_2: str = dates[1]
     routes: list[RouteModel] = get_routes()
     route_obj: RouteModel = RouteModel(name=route, description="")
     routes_list = [{"name": route.name, "description": route.description} for route in routes]
--- src/web/dashboard.py
+++ src/web/dashboard.py
@@ -4,7 +4,8 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from libRunning import RouteModel, get_routes, get_dashboard, DashboardModel
+from libRunning import (RouteModel, get_routes, get_dashboard, DashboardModel, get_sections, get_aggregations,
+                        get_dashboard_sections, get_dashboard_aggregations)
 
 from src.model.dashboard import Dashboard, get_cell
 
@@ -12,24 +13,59 @@ router = APIRouter(prefix = "/dashboard")
 templates = Jinja2Templates(directory="src/templates")
 
 
+def _create_section_and_aggregation_data(sections: list[str], aggregations: list[str],
+                                         default_sections: list[str], default_aggregations:list[str]
+                                         ) -> list[dict[str, Any]]:
+    count_: int = max(len(sections), len(aggregations))
+    items: list[dict[str, Any]] = []
+    for index in range(count_):
+        item = {}
+        try:
+            section = sections[index]
+        except IndexError:
+            section = ""
+        item["section"] = section
+        item["section_selected"] = section in default_sections
+        try:
+            aggregation = aggregations[index]
+        except IndexError:
+            aggregation = ""
+        item["aggregation"] = aggregation
+        item["aggregation_selected"] = aggregation in default_aggregations
+        items.append(item)
+    return items
+
 @router.get("", response_class=HTMLResponse)
-def show_dashboard(request: Request, route: str) -> Any:
+def show_dashboard_form(request: Request, route: str) -> Any:
     routes: list[RouteModel] = get_routes()
     route_obj: RouteModel = RouteModel(name=route, description="")
     routes_list = [{"name": route.name, "description": route.description} for route in routes]
-    dashboard_obj: DashboardModel = get_dashboard(route_obj)
+    sections = sorted(get_sections(route_obj), key=lambda x: int(x.split(".")[0]))
+    aggregations = get_aggregations(route_obj)
+    default_sections = get_dashboard_sections(route_obj)
+    default_aggregations = get_dashboard_aggregations(route_obj)
+    items: list[dict[str, Any]] = _create_section_and_aggregation_data(sections, aggregations, default_sections,
+                                                                        default_aggregations)
 
     return templates.TemplateResponse(
-        request=request, name="dashboard.html", context={"dashboard": Dashboard.create_instance(dashboard_obj), "route_name": route,
-                                                         "routes": routes_list, "get_cell": get_cell}
+        request=request, name="dashboardForm.html", context={"route_name": route, "routes": routes_list,
+                                                             "items": items}
     )
 
 @router.post("", response_class=HTMLResponse)
-def run_dashboard(request: Request, route: str) -> Any:
+async def run_dashboard(request: Request, route: str) -> Any:
+    form = await request.form()
+    sections: list[str] = []
+    aggregations: list[str] = []
+    for (k, v) in form.items():
+        if k.startswith("chks_"):
+            sections.append(v)
+        elif k.startswith("chka_"):
+            aggregations.append(v)
     routes: list[RouteModel] = get_routes()
     route_obj: RouteModel = RouteModel(name=route, description="")
     routes_list = [{"name": route.name, "description": route.description} for route in routes]
-    dashboard_obj: DashboardModel = get_dashboard(route_obj)
+    dashboard_obj: DashboardModel = get_dashboard(route_obj, sections=sections, aggregations=aggregations)
 
     return templates.TemplateResponse(
         request=request, name="dashboard.html", context={"dashboard": Dashboard.create_instance(dashboard_obj), "route_name": route,
