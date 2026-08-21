27b3ca45751bdef0b94f24d254cc5148f849d02a
Patrik Chytil <pchytil@elektroline.cz>
2026-08-21 13:36:15 +0200

    fixes due to grade from to fields are now decimals

--- src/model/grade.py
+++ src/model/grade.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from decimal import Decimal
 
 from libRunning import GradeModel
 
@@ -13,15 +14,27 @@ class Grade:
     end: str
 
     @staticmethod
+    def can_convert_to_int(value: Decimal) -> bool:
+        return value == int(value)
+
+    @staticmethod
     def convert(grades: list[GradeModel]) -> dict[int, Grade]:
         grade_dict: dict[int, Grade] = {}
         for g in grades:
             if g.time_convertible:
-                start = to_time(g.from_)
-                end = to_time(g.to_)
+                int_from = int(g.from_)
+                int_to = int(g.to_)
+
+                start = to_time(int_from)
+                end = to_time(int_to)
             else:
                 start = str(g.from_)
+                if Grade.can_convert_to_int(g.from_):
+                    start = str(int(g.from_))
+
                 end = str(g.to_)
+                if Grade.can_convert_to_int(g.to_):
+                    end = str(int(g.to_))
 
             grade = Grade(
                 g.grade,
